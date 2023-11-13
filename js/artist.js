99584caf439e326e69dbc036bd4115d8158feabf
VividerAphid <trever.kress@gmail.com>
2023-11-12 21:55:40 -0500

    Added lazy sun and moon hues + minute of hour is factored in for lighting now

--- js/artist.js
+++ js/artist.js
@@ -3,11 +3,16 @@ class artist{
         this.ctx = ctx;
         this.backColor = backCol;
         this.globalBrightness = 50; //lightness value for hsl
+        this.moonHSLA = "hsla(209, 100%, 86%, .05)";
+        this.moonColor = {ang: 209, sat: 100, bright: 86, op: .5};
+        this.sunHSLA = "hsla(50, 100%, 90%, .1)";
+        this.sunColor = {ang: 50, sat: 100, bright: 90, op: .5};
     }
     drawRock(x, y){
+        let rockCol = 45;
         this.ctx.beginPath();
-        this.ctx.fillStyle = "hsl(45, 0%,"+this.globalBrightness+"%)";
-        this.ctx.strokeStyle = "hsl(45, 0%,"+this.globalBrightness+"%)";;
+        this.ctx.fillStyle = "hsla("+ rockCol + ", 5%,"+this.globalBrightness+"%, 1)";
+        this.ctx.strokeStyle = "hsla("+ rockCol + ", 5%,"+this.globalBrightness+"%, 1)";
         this.ctx.arc(x, y, 15, 0, 2*Math.PI, false);
         this.ctx.fill();
         this.ctx.stroke();
--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var map = [];
 var arty = new artist(mappy.getContext("2d"));
 var mapDims = [mappy.width, mappy.height];
 var weather = "";
+var dat = new Date();
 
 function initMap(){
     let rockCount = randy.rangeInt(1, 50);
@@ -19,25 +20,35 @@ function calcWeather(){
 }
 
 function calcLighting(){
-    let dat = new Date();
     let hour = dat.getHours();
     let minute = dat.getMinutes();
     let brightness = 0;
     //Highest brightness at noon
     if(hour <= 12){
-        brightness = Math.round((hour/12)*50);
+        brightness = Math.round(((hour + (minute/60))/12)*50);
     }
     if(hour > 12){
-        brightness = Math.round(((23-hour) / 12)*50);
+        brightness = Math.round(((23-(hour + (minute/60))) / 12)*50);
     }
-    console.log(hour);
-    console.log(23-hour);
+    if(brightness < 15){
+        brightness = 15;
+    }
     console.log(brightness);
     arty.globalBrightness = brightness;
 }
 
 function render(){
     arty.ctx.clearRect(0, 0, mapDims[0], mapDims[1]);
+    let hour = dat.getHours();
+    if(hour > 20 || hour < 5){
+        arty.ctx.fillStyle = arty.moonHSLA;
+        arty.ctx.strokeStyle = arty.moonHSLA;
+    }
+    else{
+        arty.ctx.fillStyle = arty.sunHSLA;
+        arty.ctx.strokeStyle = arty.sunHSLA;
+    }
+    arty.ctx.fillRect(0, 0, mapDims[0], mapDims[1]);
     for(let r = 0; r < map.length; r++){
         map[r].draw();
     }
